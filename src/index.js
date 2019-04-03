85121fc1faea6b08b31033686a3f4438859e8591
Daniel Franz <kwantum0@gmail.com>
2019-04-02 22:11:59 -0700

    Completed game.
    todo
    - Move game logic to Game Component
    - Add the Time-Travel feature
    - Add Redux Reducer to tidy code

--- src/index.js
+++ src/index.js
@@ -1,55 +1,123 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-Array.prototype.mutateAt = function(i, newValue) {
+const ArrayLike = Array;
+ArrayLike.prototype.setAt = function(i, newValue) {
   let newArray = this.slice();
   newArray[i] = newValue;
-  console.log(newValue);
   return newArray;
 };
+ArrayLike.prototype.incBy = function(i, increment) {
+  let newValue = this[i] + increment;
+  return this.setAt(i, newValue);
+};
 
-const Square = ({ id, value, handleClick }) => {
+const EMPTY_GAME = ArrayLike(9).fill(null);
+const PIECES = ["O", "X"];
+const WINNING_VALUES = [
+  // rows
+  1 + 2 + 4,
+  8 + 16 + 32,
+  64 + 128 + 256,
+  // cols
+  1 + 8 + 64,
+  2 + 16 + 128,
+  4 + 32 + 256,
+  // diag
+  1 + 16 + 256,
+  4 + 16 + 64,
+];
+const checkWinner = ([yValue, xValue]) => {
+  if (WINNING_VALUES.some((winner) => (winner & yValue) === winner)) {
+    return PIECES[0];
+  }
+  if (WINNING_VALUES.some((winner) => (winner & xValue) === winner)) {
+    return PIECES[1];
+  }
+  return null;
+};
+
+const Square = ({ id, piece, disabled, handleClick }) => {
   return (
-    <button className='square' onClick={handleClick}>
-      {value}
+    <button className='square' onClick={handleClick} disabled={disabled}>
+      {piece || ""}
     </button>
   );
 };
 
 const Board = () => {
-  const [turn, setTurn] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const onSquareClick = (id) => () => {
-    console.log(`Square ${id} is clicked`);
-    setSquares(squares.mutateAt(id, turn ? "X" : "O"));
-    setTurn(!turn);
+  const [paused, setPaused] = useState(false);
+  const [status, setStatus] = useState("");
+  const [values, setValues] = useState([0, 0]);
+  const [player, setPlayer] = useState(true);
+  const [squares, setSquares] = useState(EMPTY_GAME);
+  const onSquareClick = (id, val) => () => {
+    if (!squares[id]) {
+      setSquares(squares.setAt(id, PIECES[+player]));
+      setValues(values.incBy(+player, val));
+      setPlayer(!player);
+    }
   };
-  const renderSquare = (id) => (
-    <Square id={id} value={squares[id]} handleClick={onSquareClick(id)} />
+  const restart = () => {
+    setSquares(EMPTY_GAME);
+    setValues([0, 0]);
+    setPlayer(true);
+    setPaused(false);
+  };
+
+  useEffect(() => {
+    const winner = checkWinner(values);
+    const NormalStatus = `Next player: ${PIECES[+player]}`;
+    const WinningStatus = `Player ${winner} has Won!`;
+    const DrawwingStatus = `Tie Game.`;
+
+    if (winner) {
+      setStatus(WinningStatus);
+      setPaused(true);
+    } else if (squares.every((square) => !!square)) {
+      setStatus(DrawwingStatus);
+      setPaused(true);
+    } else {
+      setStatus(NormalStatus);
+    }
+  });
+
+  const renderSquare = (id, val) => (
+    <Square
+      id={id}
+      piece={squares[id]}
+      disabled={paused}
+      handleClick={onSquareClick(id, val)}
+    />
   );
 
-  const status = "Next player: X";
+  const renderNewGame = (id, val) => (
+    <button className='new-game' hidden={!paused} onClick={restart}>
+      New Game
+    </button>
+  );
 
   return (
     <div>
       <div className='status'>{status}</div>
       <div className='board-row'>
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
+        {renderSquare(0, 1)}
+        {renderSquare(1, 2)}
+        {renderSquare(2, 4)}
       </div>
       <div className='board-row'>
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
+        {renderSquare(3, 8)}
+        {renderSquare(4, 16)}
+        {renderSquare(5, 32)}
       </div>
       <div className='board-row'>
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
+        {renderSquare(6, 64)}
+        {renderSquare(7, 128)}
+        {renderSquare(8, 256)}
       </div>
+      {renderNewGame()}
     </div>
   );
 };
